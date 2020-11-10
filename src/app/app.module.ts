5928a75f44d30f40d12dfd69deaeecc56358704e
shubham kumar <shubhamiit8298@gmail.com>
2020-11-10 22:51:54 +0530

    game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { IconComponent } from './components/icon/icon.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome"
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
+import {ToastrModule} from "ngx-toastr"
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
